a864076b782428c0ff679d60d3a3b79ff0d1c113
Arjumnr <arjumnurramadhan@gmail.com>
2023-02-15 05:05:51 +0800

    create and set up project

---
+++ app/src/main/java/com/example/apk_skp_payroll/ActivityHome.java
@@ -0,0 +1,14 @@
+package com.example.apk_skp_payroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
